30202f0c7c50e0d7a07b02ee047d397fef2e7827
santiagojorda <santiagojorda@gmail.com>
2022-10-03 09:11:51 -0300

    api refaction

--- src/api.py
+++ src/api.py
@@ -1,16 +1,6 @@
 import tweepy
-from src.credentials import API_KEY, API_SECRET, ACCESS_KEY, ACCESS_SECRET, BEARER_TOKEN
+from src.credentials import API_KEY, API_SECRET, ACCESS_KEY, ACCESS_SECRET
 
-
-# oauth2_user_handler = tweepy.OAuth2UserHandler(
-#     client_id=API_KEY 
-#     redirect_uri="Callback / Redirect URI / URL here",
-#     scope=["Scope here", "Scope here"],
-#     # Client Secret is only necessary if using a confidential client
-#     client_secret="Client Secret here"
-# )
-# client = tweepy.Client(BEARER_TOKEN)
-# api = tweepy.API(auth)
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
